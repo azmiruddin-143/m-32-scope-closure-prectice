305198661cb3665602af79b26d87c0bcb41ac181
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-09-29 10:59:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+// node index.js //
+// all undifind
+
+//  let man;
+//  console.log(man);
+
+// nan//
+// let val ;
+// let kal ;  
+// let plus = val + kal
+// console.log(plus);
+
+
+
+// let x = 10;
+
+// if(!!10){
+//     console.log("thik");
+// }
+
+// else{
+//     console.log("bul");
+// }
+
+
+// node index.js //
+// if ("2" === 2) {
+//     console.log("Inside if");
+//    } else {
+//     console.log("Inside else");
+//    }
+
+
+
+
+// Closure dunction
+// node index.js //
+
+
+ function leader() {
+     let man = 0
+     function increment (){
+         man ++
+         return man
+     }
+     return increment
+     
+ }
+
+ let final = leader();
+//  console.log(final());
+
+//  let meciyar = leader()
+//  let meciyar2 = leader()
+//  console.log(meciyar());
+//  console.log(meciyar());
+//  console.log(meciyar());
+//  console.log(meciyar());
+//  console.log(meciyar());
+//  console.log(meciyar2());
+//  console.log(meciyar2());
+//  console.log(meciyar2());
+//  console.log(meciyar());
+
+
+// 32-5 Block scope global scope simple understanding of Hoisting//
+
+// first global scope//
+
+// global scope holo akti scope er baite set kora hobe jemon {} berakate er baira varible rakha hoy ata je kono funcion
+// e call kora jay ba kaj kora jay//
+
+// block scope holo akti scope er bitore thake jemon {} berkater bitor thake and er bitore ei kaj korbe baire kaj korbe na
+// es6 onojai dorte gele let and const..
+
+// Hoisting holo varible ba function er dicretion scope er upore niye asa hoy..
+//and ata jodi varible er jonno hoy thaole sobar upre rakhle kaj korbe na undifind dekhabe.and
+// ata jodi function er jonno hoy tahole scope er upre bosale kaj korbe
+
+
